4ebe52ff023fdacf9d23b2e05bdf4e3ad4c5563f
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2019-12-24 23:25:41 +0300

    working on solution to ladder

--- src/ladder.py
+++ src/ladder.py
@@ -1,30 +1,33 @@
-import itertools
+def ladder_rec(values):
+    val_len = len(values)
+    calcs = [None] * len(values)
 
-def ladder(values):
-    result = 0
-    length = len(values)
-    i = 0
-    while i < length:
-        if i == length - 1 or i == length - 2:
-            result += values[i]
+    def inner(index, total):
+        if index == val_len:
+            return total
+        elif index + 1 == val_len:
+            calcs[index] = values[index]
+            return inner(index + 1, total + values[index])
         else:
-            current_value = values[i]
-            next_value = values[i + 1]
-            if result + next_value > result + current_value:
-                i += 1
-                current_value = next_value
-            result += current_value
-        i += 1
+            one_step = calcs[index]
+            if one_step is not None:
+                if calcs[index] else inner(index + 1)
+            calcs[index] = one_step
+            two_step = calcs[index + 1] if calcs[index + 1] else inner(index + 2)
+            calcs[index] = two_step
+            return inner(one_step if one_step > two_step else two_step)
 
-    return result
+    return inner(0, 0)
 
+
+# Подсказка: можно создать вектор D, в котором D[i] - максимальная сумма первых i чисел,
+# и на каждой итерации брать максимум из двух вариантов перехода по лестнице.
 
 def main():
-    _ = int(input())
+    # _ = int(input())
     # values = list(map(int, input().split()))
-    values = input().split()
-    # result = ladder(values)
-    result = list(itertools.combinations(values, 2))
+    values = [1, 1, -2, -4, -6, 2, 2]
+    result = ladder_rec(values)
     print(result)
 
 
